d3216b35e8303e63157c32cf6a7297c3cd7b71d1
siberia <>
2020-12-02 16:58:22 +0800

    引入SVG图标

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
+  // Link,
   Redirect,
 } from "react-router-dom"
 import "./App.css"
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,30 +2,51 @@ import styled from "styled-components"
 import { Link } from "react-router-dom"
 import React from "react"
 
+require("icons/money.svg")
+require("icons/statistics.svg")
+require("icons/tag.svg")
+
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   > ul {
     display: flex;
+    padding: 5px 0 0 0;
     > li {
+      font-size: 10px;
       width: 33.333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      > .icon {
+        width: 30px;
+        height: 30px;
+      }
     }
   }
 `
 
 const Nav = () => {
-  return(
+  return (
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签🏷️</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账🧾</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics" />
+          </svg>
           <Link to="/statistics">统计📈</Link>
         </li>
       </ul>

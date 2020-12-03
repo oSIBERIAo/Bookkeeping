23eab3a90dc3694b6432c89dd31f1a9fc31735ed
siberia <>
2020-12-03 16:01:29 +0800

    Money 的 HTML (Money.tsx 误写为 Tags.tsx)

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,48 @@
 import React from "react"
 import { Layout } from "../components/Layout"
+import styled from "styled-components"
+
+const TagsSection = styled.section``
+const NotesSection = styled.section``
+const NumberPadSection = styled.section``
 
 const Tags = () => {
     return (
         <Layout>
-            <h2>Tags</h2>
+            <TagsSection>
+                <ol>
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ol>
+                <button>新增标签</button>
+            </TagsSection>
+            <NotesSection>
+                <label htmlFor="">
+                    <span>备注</span>
+                    <input type="text" name="" id="" />
+                </label>
+            </NotesSection>
+            <NumberPadSection>
+                <div>100</div>
+                <div>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>删除</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清空</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>OK</button>
+                    <button>0</button>
+                    <button>.</button>
+                </div>
+            </NumberPadSection>
         </Layout>
     )
 }

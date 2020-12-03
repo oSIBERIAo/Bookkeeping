b1f41a2e7c0f457f0667e5221b6c36db2fa023d8
siberia <>
2020-12-03 16:52:45 +0800

    Money.tsx > TagsSection CSS 样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -2,7 +2,29 @@ import React from "react"
 import { Layout } from "../components/Layout"
 import styled from "styled-components"
 
-const TagsSection = styled.section``
+const TagsSection = styled.section`
+    background-color: #fff;
+    padding: 12px 16px;
+    ol {
+        margin: 0 -12px;
+        li {
+            background-color: #d9d9d9;
+            display: inline-block;
+            border-radius: 18px;
+            padding: 3px 16px;
+            font-size: 14px;
+            margin: 4px 12px;
+        }
+    }
+    button {
+        background: none;
+        border: none;
+        padding: 2px 4px;
+        border-bottom: 1px solid #666;
+        margin-top: 8px;
+        color: #666;
+    }
+`
 const NotesSection = styled.section``
 const NumberPadSection = styled.section``
 
